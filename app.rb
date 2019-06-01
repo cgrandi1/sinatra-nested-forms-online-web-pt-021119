198b08dd7b6c19dd510ea82c9684463dbfa3094d
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-01 23:35:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      binding.pry
+    end
 
   end
 end
